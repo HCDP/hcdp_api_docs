8a02a9b5c8fd4404f1a9c440c3783177d0a756dc
marcusp478 <marcusprudencio03@gmail.com>
2025-07-15 17:56:43 -1000

    Try it out disabled for POST methods only

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,12 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    //url: "swagger.yaml",
-    urls: [
-      { url: "full-list.yaml", name: "Full API Endpoint List" },
-      { url: "try-it-out.yaml", name: "Interactive Docs" }
-    ],
-    "urls.primaryName": "Full API Endpoint List",
+    url: "hcdp_api.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
@@ -18,39 +13,9 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    supportedSubmitMethods: [],
+    supportedSubmitMethods: ["get"],
     layout: "StandaloneLayout"
   });
-
-  const observer = new MutationObserver(() => {
-    const uiReady =
-      window.ui &&
-      window.ui.specActions &&
-      typeof window.ui.specActions.updateUrl === 'function';
-
-    if (!uiReady) return;
-
-    observer.disconnect();
-
-    const updateUrl = window.ui.specActions.updateUrl;
-
-    // Function override
-    window.ui.specActions.updateUrl = function (url) {
-
-      if (url.includes("full-list")) {
-        window.ui.getConfigs().supportedSubmitMethods = [];
-      } else {
-        window.ui.getConfigs().supportedSubmitMethods = ["get", "post"];
-      }
-
-      return updateUrl.apply(this, arguments);
-    }
-  });
-
-  const swaggerUI = document.getElementById('swagger-ui');
-  if (swaggerUI) {
-    observer.observe(swaggerUI, { childList: true, subtree: true });
-  }
 
   //</editor-fold>
 };
